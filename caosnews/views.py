a37260df5f414d0e2cdc5d47ada91a94cc9cb46a
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-06-24 01:39:03 -0400

    feat: creación vista home

--- caosnews/views.py
+++ caosnews/views.py
@@ -1,3 +1,31 @@
+import random
+
+from django.core.paginator import Paginator, EmptyPage
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Publicacion
+
+sin_noticias = {
+    "titulo": "No hay noticias hoy!"
+}
+
+
+def home(request):
+    posts = Publicacion.filter_from_cache(publicada=True)
+    try:
+        paginator = Paginator(posts, 2)
+        numero_pagina = request.GET.get("page")
+        noticias = paginator.get_page(numero_pagina)
+    except EmptyPage:
+        # noticia_caliente = sin_noticias
+        noticias = [sin_noticias]
+    # else:
+    #     noticia_caliente = posts[random.randrange(0, len(posts))]
+
+    contexto = {
+        # "noticia_caliente": noticia_caliente,
+        "posts": noticias,
+        "title": "Home"
+    }
+
+    return render(request, 'sitio/home.html', contexto)
