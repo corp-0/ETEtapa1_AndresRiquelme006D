35cf7beac307ceffe338a93f10cda8e7a9ae728f
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-06-24 01:40:13 -0400

    misc: corrige librería para envs

--- manage.py
+++ manage.py
@@ -2,13 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from dotenv import load_env
+from dotenv import load_dotenv
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ETEtapa1_AndresRiquelme006D.settings')
-    load_env()
+    load_dotenv()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
