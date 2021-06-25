77a2b2b6913f032b950402d9ed272074a8557027
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-06-25 00:03:52 -0400

    refactor: optimiza script de poblado de datos de prueba

--- caosnews/management/commands/generarusuarios.py
+++ caosnews/management/commands/generarusuarios.py
@@ -24,49 +24,79 @@ apellidos = [
 
 from django.core.management.base import BaseCommand
 from caosnews.models import Usuario, Colaborador
-from caosnews.usuario import Tipo
+from caosnews.tipo_usuario import Tipo
+
+
+class UsuarioFalso:
+    incremental: int
+    nombre: str
+    apellido: str
+    nombre_completo: str
+    username: str
+    email: str
+    tipo: Tipo
+    es_colaborador: bool
+    foto: str
+    rut: str
+    fono: str
+    direccion: str
+    pais: str
+
+    def __init__(self, incremental, es_colab):
+        self.incremental = incremental
+        self.es_colaborador = es_colab
+        self.generar()
+
+    def generar(self):
+        self.nombre = random.choice(nombres)
+        self.apellido = random.choice(apellidos)
+        self.nombre_completo = f"{self.nombre} {self.apellido}"
+        self.username = f"{self.nombre}{self.incremental}"
+        self.email = f"{self.nombre}{self.apellido}@correo.cl"
+        self.tipo = Tipo.COLABORADOR.value if self.es_colaborador else Tipo.EDITOR.value
+        self.foto = "foto.jpg"
+        self.rut = "99.999.999-9"
+        self.fono = "+56990099999"
+        self.direccion = "Avenida mi casa 1234"
+        self.pais = "Chile"
+
+        return self
 
 
 class Command(BaseCommand):
     help = "Crea una cantidad de usuarios de prueba"
-    incremental: int = 0
 
     def add_arguments(self, parser):
         parser.add_argument('cantidad', type=int)
 
     def handle(self, *args, **options):
         print("generando usuarios...")
+        fakes = []
+        usuarios = []
+        colabs = []
         for i in range(0, options.get('cantidad', 100)):
-            self.generar()
+            fakes.append(UsuarioFalso(i, random.random() < 0.8))
 
-    def generar(self):
-        self.incremental += 1
-        nombre = random.choice(nombres)
-        apellido = random.choice(apellidos)
-        username = f"{nombre}{self.incremental}"
-        email = f"{nombre}{apellido}@correo.cl"
-        tipo = Tipo.EDITOR.value
-        es_colaborador = random.random() < 0.8
-        if es_colaborador:
-            tipo = Tipo.COLABORADOR.value
+        for f in fakes:
+            u = Usuario(username=f.username, email=f.email, tipo_usuario=f.tipo)
+            u.set_unusable_password()
+            usuarios.append(u)
 
-        usuario = Usuario(username=username, email=email, tipo_usuario=tipo)
-        usuario.set_password(f"{nombre + apellido[-2:]}{self.incremental * 10}")
-        usuario.save()
+        Usuario.objects.bulk_create(usuarios)
+        usuarios = Usuario.objects.all()
 
-        if es_colaborador:
-            colab = Colaborador(
-                user=usuario,
-                rut="99.999.999-9",
-                nombre=f"{nombre} {apellido}",
-                foto="foto",
-                fono="569999999",
-                direccion="Avenida mi casa 1234",
-                pais="Chile"
+        for index, f in enumerate(fakes):
+            if f.es_colaborador:
+                c = Colaborador(
+                    user=usuarios[index],
+                    rut=f.rut,
+                    foto=f.foto,
+                    nombre=f.nombre_completo,
+                    fono=f.fono,
+                    direccion=f.direccion,
+                    pais=f.pais
                 )
-            colab.save()
 
+                colabs.append(c)
 
-
-
-
+        Colaborador.objects.bulk_create(colabs)
