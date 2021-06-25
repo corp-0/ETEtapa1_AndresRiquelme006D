9242ba1fdbd52f65a2d3a90a40e41a3396473bc5
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-06-25 16:52:20 -0400

    feat: agregada vista de noticia y autor

--- caosnews/urls.py
+++ caosnews/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import (
-    Home
+    Home,
+    noticia,
+    Autor,
 )
 
 urlpatterns = [
-    path("", Home.as_view(), name="home")
+    path("", Home.as_view(), name="home"),
+    path("noticia/<noticia_id>", noticia, name="noticia"),
+    path("autor/<autor_id>", Autor.as_view(), name="autor")
 ]
--- caosnews/views.py
+++ caosnews/views.py
@@ -1,12 +1,13 @@
+import json
+import os
+from functools import lru_cache
+from urllib import request
+
 from django.db.models import Max
 from django.views.generic import ListView
-from urllib import request
-from django.utils.functional import cached_property
-import os
-from .models import Publicacion
-import json
+from django.shortcuts import render, HttpResponse
 
-from functools import lru_cache
+from .models import Publicacion, Colaborador
 
 
 @lru_cache(maxsize=32)
@@ -41,3 +42,44 @@ class Home(ListView):
         context["el_clima"] = obtener_clima()
 
         return context
+
+
+def noticia(request, noticia_id):
+    pub = Publicacion.objects.get(pk=noticia_id)
+    # TODO Horrible forma de contar visitas, no verifica si son únicas
+    pub.visitas += 1
+    pub.save()
+
+    mas_noticias = Publicacion.objects.filter(publicada=True, autor=pub.autor_id).exclude(pk=pub.id)[:3]
+
+    contexto = {
+        "noticia": pub,
+        "el_clima": obtener_clima(),
+        "title": pub.titulo,
+        "mas_noticias": mas_noticias
+    }
+
+    return render(request, 'sitio/noticia.html', contexto)
+
+
+class Autor(ListView):
+    template_name = "sitio/autor.html"
+    context_object_name = "posts"
+    paginate_by = 10
+    ordering = "-fecha"
+
+    def get_queryset(self):
+        return Publicacion.objects.filter(publicada=True, autor=self.kwargs.get('autor_id', None))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(Autor, self).get_context_data(**kwargs)
+        autor = Colaborador.objects.get(user_id=self.kwargs.get("autor_id", None))
+        noticias = Publicacion.objects.filter(autor=self.kwargs.get('autor_id', None))
+        context["title"] = "Noticias de " + autor.nombre
+        context["autor"] = autor
+        context["total_noticias"] = noticias.count()
+        context["noticias_publicadas"] = noticias.exclude(publicada=False).count()
+
+        return context
+
+
